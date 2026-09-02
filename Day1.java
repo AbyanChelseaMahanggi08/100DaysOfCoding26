ecb730f9fd7c9fdcacd26fab02c536e85bec0a8d
AbyanChelseaMahanggi08 <abyanchelsea45@gmail.com>
2026-09-02 07:39:57 -0700

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,15 @@
+/**
+ * Contoh struktur dasar program Java.
+ */
+
+ /* Nama class harus sama dengan nama file .java */
+public class Day1{
+
+ // ini METHOD UTAMA - Titik awal program dijalankan
+public static void main(String[] args) {
+
+    // Perintah dalam bahasa Java untuk menampilkan output
+    System.out.println("Hello, world");
+    
+}  
+}
