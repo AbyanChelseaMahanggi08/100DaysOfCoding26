4e20e1e2dc2bf50beaf985633af0a61ad9f8e464
Abyan Chelsea mahanggi <abyanchelsea45@gmail.com>
2026-09-04 02:38:20 -0700

    Update escape sequences in Day3.java output

---
+++ Day3.java
@@ -0,0 +1,32 @@
+//Program Mempelajari Penggunaan Escape Squences
+
+public class Day3 {
+ public static void main(String[] args) {
+ 
+ 
+ // \n (NEWLINE) Digunakan untuk membuat garis baru 
+ System.out.println("LIST BARANG YG INGIN DIBELI :\n1.BAJU\n2.CELANA\n3.SEPATU");
+ 
+ /* 
+ * \t (TAB) Digunakan untuk membuat tab atau memberikan jarak horizontal agar
+ * tampilan teks lebih rapi dan dapat terlihat sejajar ke samping.
+ */ 
+ System.out.println("NAMA\t: ABYAN\nUMUR\t: 18\nALAMAT\t: Limboro ");
+
+ // \"  Digunakan untuk menambahkan TANDA PETIK DUA pada teks
+ System.out.println("\"Hello World\"");
+
+ // \' Digunakan untuk menambahkan TANDA PETIK SATU pada teks
+ System.out.println("\'Halo Dunia\'");
+
+ // \\ (BACKSLASH) Digunakan untuk menampilkan karakter backslash
+ System.out.println("Folder C:\\Pictures\\Foto");
+
+ // \b (BACKSPACE) Digunakan untuk menghapus 1 karakter sebelumnnya
+ System.out.println("AA\bbyan CC\bhelsea MM\bahanggi");
+
+ // \r (Carriage Return) Digunakan untuk mengembalikan posisi kursor ke awal baris
+ System.out.println("Prodi\rInformatika");
+
+ }
+}
