12c2154d9511e369b607035e1d03d5d7c5e1beb3
Abyan Chelsea mahanggi <abyanchelsea45@gmail.com>
2026-09-06 22:50:29 -0700

    Add Day6.java to demonstrate char and boolean types
    This program demonstrates the use of character and boolean data types in Java.

---
+++ Day6.java
@@ -0,0 +1,21 @@
+// Mempelajari Tipe Data Karakter //Program mempe(char) dan Boolean (boolean)
+
+public class Day6 {
+ public static void main(String[] args) {
+
+ // char digunakan untuk menyimpan satu karakter.   
+ char inisial = 'A';
+ char simbol = '#';
+ 
+ // boolean digunakan untuk menyimpan dua nilai yaitu, TRUE/FALSE (BENAR/SALAH)
+ boolean sudahMakan = true;
+ boolean sudahMinum = false; 
+
+ // Hasil dari kedua tipe data yang kita buat
+ System.out.println("Inisial = " + inisial);
+ System.out.println("Simbol = " + simbol);
+
+ System.out.println("Sudah Makan = " + sudahMakan);
+ System.out.println("Sudah Minum = " + sudahMinum);
+ }
+}
