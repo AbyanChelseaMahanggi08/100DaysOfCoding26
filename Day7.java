e80b4fae20ed02b9d5e5efeec15f1fd27e19844f
Abyan Chelsea mahanggi <abyanchelsea45@gmail.com>
2026-09-07 17:37:04 -0700

    Add Day7.java to demonstrate String data type
    This program introduces the String data type and demonstrates its usage by storing and displaying personal information.

---
+++ Day7.java
@@ -0,0 +1,23 @@
+//Program Pengenalan Tipe Data String
+public class Day7 {
+ public static void main(String[] args) {
+  
+ /*
+ * String digunakan untuk menyimpan dan mengelola data yang berupa tesk atau kumpulan
+ * karakter dalam sebuah program. Tipe data ini dapat digunakan untuk menyimpan berbagai
+ * informasi seperti nama, alamat, pesan, kode barang, DLL. 
+ */   
+ String Nama = "Abyan Chelsea Mahanggi";  
+ String Alamat = "Kecamatan Limboro";
+ String Prodi = "Informatika";
+ String AsalSekolah = "UPTD SMA NEGERI 1 TINAMBUNG"; 
+ String Pesan = "Salam Kenal Teman-Teman";
+
+ // Menampilkan hasil
+ System.out.println("Perkenalkan Nama saya \t= " + Nama);
+ System.out.println("Saya berasal dari \t= " + Alamat);
+ System.out.println("Saya dari prodi \t= " + Prodi);
+ System.out.println("Saya Alumni \t\t= " + AsalSekolah);
+ System.out.println("Pesan \t\t\t= " + Pesan);
+ }
+}
