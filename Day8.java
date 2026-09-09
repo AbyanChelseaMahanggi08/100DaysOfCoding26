0dcb91ac3633a52e1c80cc1f94dae8d04355ec10
Abyan Chelsea mahanggi <abyanchelsea45@gmail.com>
2026-09-09 02:31:31 -0700

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,19 @@
+// Pengenalan Deklarasi, Inisialisasi Dan Update Variable
+
+public class Day8 {
+ public static void main(String[] args) {
+
+ // Deklarasi digunakan untuk membuat sebuah variable dengan tipe data tertentu
+ int Nilai;
+
+ // Inisialisasi digunakan untuk memberikan nilai awal
+ Nilai = 75;
+ System.out.println("Nilai Ujian Awal = " + Nilai);
+
+ // Update digunakan untuk mengupdate nilai awal menjadi nilai yang baru
+ Nilai = 90;
+ System.out.println("Nilai Ujian Baru = " + Nilai);
+
+
+ }
+}
