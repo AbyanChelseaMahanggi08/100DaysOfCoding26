b2ab7c5839ee57a16c64382673eb988b928322f1
Abyan Chelsea mahanggi <abyanchelsea45@gmail.com>
2026-09-09 18:28:44 -0700

    Add Day9.java to demonstrate constant variables

---
+++ Day9.java
@@ -0,0 +1,25 @@
+// Program pengenalan Constanta (Final Variable)
+public class Day9 {
+public static void main(String[] args) {
+
+  
+/*
+* Konstanta adalah nilai yang tidak dapat diubah setelah ditetapkan. Konstanta biasanya
+* digunakan untuk menyimpan nilai yang tetap selama program berjalan. 
+*/   
+
+// Contoh menggunakan String
+final String PRODI = "INFORMATIKA";
+System.out.println("PRODI\t\t\t : " + PRODI);
+
+// Contoh menggunakan int
+final int TAHUN_SEKARANG = 2026;
+System.out.println("SEKARANG TAHUN BERAPA\t : " + TAHUN_SEKARANG);
+
+// Contoh menggunakan double
+final double TINGGI_BADAN = 170.5;
+System.out.println("TINGGI SAYA\t\t : " + TINGGI_BADAN + "CM");
+
+
+}    
+}
