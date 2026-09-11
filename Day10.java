e59701446ae58ca5eeb942114202e7e68c11c7f3
Abyan Chelsea mahanggi <abyanchelsea45@gmail.com>
2026-09-11 04:29:43 -0700

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,40 @@
+public class Day10 {
+public static void main(String[] args) {
+    
+// BYTE
+System.out.println("*BYTE*");
+System.out.println("BYTE MIN\t : " + Byte.MIN_VALUE);
+System.out.println("BYTE MAX\t : " + Byte.MAX_VALUE);
+
+// SHORT
+System.out.println("*SHORT*");
+System.out.println("SHORT MIN\t : " + Short.MIN_VALUE);
+System.out.println("SHORT MAX\t : " + Short.MAX_VALUE);
+
+// INT
+System.out.println("*INT*");
+System.out.println("INT MIN\t\t : " + Integer.MIN_VALUE);
+System.out.println("INT MAX\t\t : " + Integer.MAX_VALUE);
+
+// LONG
+System.out.println("*LONG*");
+System.out.println("LONG MIN\t : " + Long.MIN_VALUE);
+System.out.println("LONG MAX\t : " + Long.MAX_VALUE);
+
+// FLOAT
+System.out.println("*FLOAT*");
+System.out.println("FLOAT MIN\t : " + Float.MIN_VALUE);
+System.out.println("FLOAT MAX\t : " + Float.MAX_VALUE);
+
+// DOUBLE
+System.out.println("*DOUBLE*");
+System.out.println("DOUBLE MIN\t : " + Double.MIN_VALUE);
+System.out.println("DOUBLE MAX\t : " + Double.MAX_VALUE);
+
+// CHAR
+System.out.println("*CHAR*");
+System.out.println("CHAR MIN\t : " + (int) Character.MIN_VALUE);
+System.out.println("CHAR MAX\t : " + (int) Character.MAX_VALUE);
+
+}    
+}
