197481b1eca14759a39aa9c821dc9a647d4762f5
Abyan Chelsea mahanggi <abyanchelsea45@gmail.com>
2026-09-15 03:57:16 -0700

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,17 @@
+public class Day14 {
+public static void main(String[] args) {
+ 
+// Operator Aritmatika Penjumlahan dan Pengurangan   
+
+int angka1 = 5;
+int angka2 = 15;
+int angka3 = 30;
+
+int hasilPenjumlahan = angka1 + angka2 + angka3;
+int hasilPengurangan = angka3 - angka2 - angka1;
+
+System.out.println("Hasil Penjumlahan = " + hasilPenjumlahan);
+System.out.println("Hasil Pengurangan  = " + hasilPengurangan);
+    
+}
+}
