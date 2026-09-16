d95ae0ebe2429881a6d1c194e6701c4fa4b16dfe
Abyan Chelsea mahanggi <abyanchelsea45@gmail.com>
2026-09-16 05:39:18 -0700

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,16 @@
+public class Day15{
+public static void main(String[] args) {
+    
+// Operator Aritmatika Perkalian dan Pembagian
+ 
+int angka1 = 100;
+int angka2 = 2;
+ 
+int hasilPerkalian = angka1*angka2;
+int hasilPembagian = angka1/angka2; 
+
+System.out.println("Hasil Perkalian = " + hasilPerkalian);
+System.out.println("Hasil Pembagian = " + hasilPembagian);
+
+}
+}
