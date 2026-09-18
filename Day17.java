4ab4939ed334592f96687143041313b7743f21ec
Abyan Chelsea mahanggi <abyanchelsea45@gmail.com>
2026-09-18 05:54:16 -0700

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,29 @@
+public class Day17{
+public static void main(String[] args) {
+
+// Operator Penugasan +=, -=, *=, /=, %=
+
+int a = 50;
+
+// Menambahkan nilai a dengan 20
+a += 20;
+System.out.println("Hasil a += 50 = " + a);
+
+// Mengurangi nilai a dengan 20
+a -= 20;
+System.out.println("Hasil a -= 20 = " + a);
+
+// Mengkalikan nilai a dengan 2
+a *= 2;
+System.out.println("Hasil a *= 2  = " + a);
+
+// Membagi nilai a dengan 10
+a /= 10;
+System.out.println("Hasil a /= 10 = " + a);
+
+// Mencari sisa bagi nilai a dengan 3
+a %= 3;
+System.out.println("Hasil a %= 3  = " + a);
+
+}
+}
