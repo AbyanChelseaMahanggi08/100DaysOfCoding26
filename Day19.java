c84d12d2588e0422b1f652d9ed813e7144712ac1
Abyan Chelsea mahanggi <abyanchelsea45@gmail.com>
2026-09-20 03:52:10 -0700

    Add Day19.java for manual number conversion

---
+++ Day19.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Day19 {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+// Konversi Manual
+
+System.out.print("Masukkan Angka : ");
+double a = sc.nextDouble();
+
+int b = (int) a;
+
+System.out.println("\n======= HASIL =======");
+System.out.println("Nilai Double\t : " + a);
+System.out.println("Nilai Int\t : " + b);
+
+
+}
+}
